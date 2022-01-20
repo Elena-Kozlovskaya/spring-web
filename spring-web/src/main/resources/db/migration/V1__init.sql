9411739ceb8cc3e104389ca97a8c57b617ccd535
Elena Kozlovskaya <lenadk23@gmail.com>
2022-01-20 17:32:03 +0500

    Added homework lesson_3

--- spring-web/src/main/resources/db/migration/V1__init.sql
+++ spring-web/src/main/resources/db/migration/V1__init.sql
@@ -1,33 +1,46 @@
-create table products
+create table categories
 (
-    id         bigserial primary key,
-    title      varchar(255),
-    price      int,
-    created_at timestamp default current_timestamp,
-    updated_at timestamp default current_timestamp
+    id          bigserial primary key,
+    title       varchar(255),
+    created_at  timestamp default current_timestamp,
+    updated_at  timestamp default current_timestamp
 );
 
-insert into products (title, price)
-values ('Potato', 2),
-       ('Carrot', 4),
-       ('Pepper', 8),
-       ('Tomato', 16),
-       ('Asparagus', 75),
-       ('Beans', 23),
-       ('Beet', 12),
-       ('Broccoli', 45),
-       ('Cabbage ', 21),
-       ('Cauliflower ', 47),
-       ('Celery ', 38),
-       ('Corn', 33),
-       ('Cucumber ', 145),
-       ('Daikon', 97),
-       ('Eggplant', 123),
-       ('Garlic', 67),
-       ('Kale ', 77),
-       ('Lettuce ', 68),
-       ('Onion ', 25),
-       ('Parsley', 27);
+create table products
+(
+    id            bigserial primary key,
+    title         varchar(255),
+    price         int,
+    category_id   bigint not null references categories (id),
+    created_at    timestamp default current_timestamp,
+    updated_at    timestamp default current_timestamp
+);
+
+insert into categories (title)
+values ('Vegetables'),
+       ('Fruit');
+
+insert into products (title, price, category_id)
+values ('Potato', 2, 1),
+       ('Carrot', 4, 1),
+       ('Pepper', 8, 1),
+       ('Tomato', 16, 1),
+       ('Asparagus', 75, 1),
+       ('Apple', 23, 2),
+       ('Beet', 12, 1),
+       ('Orange', 45, 2),
+       ('Cabbage', 21, 1),
+       ('Grape', 47, 2),
+       ('Celery', 38, 1),
+       ('Corn', 33, 1),
+       ('Cucumber', 145, 1),
+       ('Daikon', 97, 1),
+       ('Eggplant', 123, 1),
+       ('Garlic', 67, 1),
+       ('Kale', 77, 1),
+       ('Lettuce', 68, 1),
+       ('Onion', 25, 1),
+       ('Parsley', 27, 1);
 
 create table users
 (
