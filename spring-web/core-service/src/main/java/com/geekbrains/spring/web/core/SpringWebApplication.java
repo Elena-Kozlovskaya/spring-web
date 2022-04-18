1fbd4415e3934a975e28c2fc1d456b11e3717e4b
Elena Kozlovskaya <lenadk23@gmail.com>
2022-04-18 12:44:43 +0500

    Added cache in ProductService

--- spring-web/core-service/src/main/java/com/geekbrains/spring/web/core/SpringWebApplication.java
+++ spring-web/core-service/src/main/java/com/geekbrains/spring/web/core/SpringWebApplication.java
@@ -2,10 +2,12 @@ package com.geekbrains.spring.web.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("secret.properties")
+@PropertySource("classpath:secret.properties")
+@EnableCaching
 public class SpringWebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebApplication.class, args);
---
+++ spring-web/core-service/src/main/java/com/geekbrains/spring/web/core/configs/CacheConfig.java
@@ -0,0 +1,30 @@
+package com.geekbrains.spring.web.core.configs;
+
+import com.google.common.cache.CacheBuilder;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class CacheConfig {
+
+    @Bean("CacheManager")
+    public CacheManager cacheManager() {
+        return new ConcurrentMapCacheManager() {
+            @Override
+            protected Cache createConcurrentMapCache(String name) {
+                return new ConcurrentMapCache(
+                        name,
+                        CacheBuilder.newBuilder()
+                                .expireAfterWrite(1440, TimeUnit.MINUTES)
+                                .build().asMap(),
+                        false);
+            }
+        };
+    }
+}
